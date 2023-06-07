7103504cd5d3c3791beb36d379453225a51eff89
Alicja Gruca <alicjagruca6@gmail.com>
2023-06-07 22:52:06 +0200

    Initial Commit

---
+++ TopMovies/TopMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopMoviesApp.swift
+//  TopMovies
+//
+//  Created by Alicja Gruca on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TopMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
