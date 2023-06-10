f572947e133280c044a83e7522c477d2f7463545
Alicja Gruca <alicjagruca6@gmail.com>
2023-06-11 01:26:27 +0200

    :sparkles: Create data model and api service

---
+++ TopMovies/Api/Service/MovieService.swift
@@ -0,0 +1,64 @@
+//
+//  MovieService.swift
+//  TopMovies
+//
+//  Created by Alicja Gruca on 10/06/2023.
+//
+
+import Foundation
+import Alamofire
+
+class MovieService {
+    let baseUrlString = Constants.baseUrl
+        let headers: HTTPHeaders = [
+            "X-RapidAPI-Key": Constants.apiKey,
+            "X-RapidAPI-Host": Constants.apiHost
+        ]
+
+        // Fetch list of movies
+        func fetchMovies(completion: @escaping ([Movie]?) -> Void) {
+            AF.request(baseUrlString, headers: headers).responseDecodable(of: [Movie].self) { response in
+                switch response.result {
+                case .success:
+                    if let data = response.data {
+                        do {
+                            let movies = try JSONDecoder().decode([Movie].self, from: data)
+                            completion(movies)
+                        } catch {
+                            print("Error decoding movies: \(error.localizedDescription)")
+                            completion(nil)
+                        }
+                    } else {
+                        completion(nil)
+                    }
+                case .failure(let error):
+                    print("Error: \(error.localizedDescription)")
+                    completion(nil)
+                }
+            }
+        }
+
+        // Fetch a single movie
+        func fetchMovie(movieID: String, completion: @escaping (Movie?) -> Void) {
+            let singleMovieURLString = "\(baseUrlString)/\(movieID)"
+            AF.request(singleMovieURLString, headers: headers).responseDecodable(of: Movie.self) { response in
+                switch response.result {
+                case .success:
+                    if let data = response.data {
+                        do {
+                            let movie = try JSONDecoder().decode(Movie.self, from: data)
+                            completion(movie)
+                        } catch {
+                            print("Error decoding movie: \(error.localizedDescription)")
+                            completion(nil)
+                        }
+                    } else {
+                        completion(nil)
+                    }
+                case .failure(let error):
+                    print("Error: \(error.localizedDescription)")
+                    completion(nil)
+                }
+            }
+        }
+}
