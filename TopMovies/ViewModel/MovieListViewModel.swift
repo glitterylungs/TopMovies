310ab11374e9b1d573988c28a42588c159c51cbc
Alicja Gruca <alicjagruca6@gmail.com>
2023-06-11 02:14:40 +0200

    :sparkles: Add movie list

---
+++ TopMovies/ViewModel/MovieListViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  MovieListViewModel.swift
+//  TopMovies
+//
+//  Created by Alicja Gruca on 11/06/2023.
+//
+
+import Foundation
+import Alamofire
+
+class MovieListViewModel: ObservableObject {
+    
+    private let movieService = MovieService()
+    
+    @Published var movies: [Movie] = []
+
+    func fetchMovies() {
+        movieService.getMovies { [weak self] movies in
+            DispatchQueue.main.async {
+                self?.movies = movies ?? []
+            }
+        }
+    }
+}
