d478245c9ca814687642598883c91e1e3facce84
Alicja Gruca <alicjagruca6@gmail.com>
2023-06-15 20:13:49 +0200

    :lispstick: :sparkles: Add shaking, design the list

---
+++ TopMovies/Extension/DeviceExtension.swift
@@ -0,0 +1,13 @@
+//
+//  DeviceExtension.swift
+//  TopMovies
+//
+//  Created by Alicja Gruca on 15/06/2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIDevice {
+    static let deviceDidShakeNotification = Notification.Name(rawValue: "deviceDidShakeNotification")
+}
--- TopMovies/View/MovieListView.swift
+++ TopMovies/View/MovieListView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct MovieListView: View {
     @ObservedObject private var viewModel = MovieListViewModel()
     
+    @State private var randomMovieID: String? = nil
+        @State private var shouldNavigate = false
+    
     var body: some View {
         NavigationView {
             if viewModel.movies.isEmpty {
@@ -21,14 +24,34 @@ struct MovieListView: View {
                 List(viewModel.movies, id: \.id) { movie in
                     NavigationLink(destination: MovieDetailsView(movieID: movie.id)) {
                         HStack {
-                            Text(String(movie.rank))
-                            AsyncImage(url: URL(string: movie.thumbnail))
+                            Text(String(movie.rank)).font(.headline).fontWeight(.medium)
+                            AsyncImage(url: URL(string: movie.thumbnail)){ image in
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                            } placeholder: {
+                                ProgressView()
+                            }.frame(width: 50).padding(.horizontal, 10)
                             Text(movie.title)
                         }
+                    }.onShake {
+                        guard !shouldNavigate else { return }
+                        randomMovieID = "top\(Int.random(in: 1...100))"
+                        shouldNavigate = true
                     }
+                    .background(
+                        NavigationLink(
+                            destination: MovieDetailsView(movieID: randomMovieID ?? ""),
+                            isActive: $shouldNavigate,
+                            label: { EmptyView() }
+                        )
+                        .hidden()
+                    )
                 }
                 .navigationTitle("Top 100 Movies")
                 .listStyle(.inset)
+
             }
         }.accentColor(Color("Text"))
     }
